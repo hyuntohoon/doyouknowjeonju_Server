4f48fa74c552708682498bd78865655440ebf84c
kim ji youn <gkwovlf123@naver.com>
2022-10-08 01:28:33 +0900

    모든 주차장데이터를 json 형태로 불러옴

--- routes/parkingRoutes.js
+++ routes/parkingRoutes.js
@@ -1,6 +1,30 @@
 const express = require("express");
+const mysql = require("mysql");
 const parkingController = require("./../controllers/parkingController");
-
 const router = express.Router();
+
+router.get('/get_All', function(req, res, next) {
+    parkingController.get_Allparking(
+      (err)=> {
+        return next(err);
+      },
+      (rows)=> {
+        if(rows!=" "){
+        return res.json({
+          isSuccess: true,
+          code: 200,
+          message: "조회성공",
+          data:rows 
+        });
+      }
+      else {
+        return res.json({
+          isSuccess: false,
+          code: 300,
+          message: "조회싪패", 
+        })
+      }
+  });
+});
 
 module.exports = router;
