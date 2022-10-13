03c326d102744d248df740c3f01f5c6a2990eb99
kim ji youn <gkwovlf123@naver.com>
2022-10-13 20:55:52 +0900

    진료과목별 병원 라우터 추가

--- controllers/hospitalController.js
+++ controllers/hospitalController.js
@@ -40,3 +40,115 @@ exports.getAllhospital10000000 = (fail, done) => {
         });
     });
 };
+
+exports.getAllhospital01000000 = (fail, done) => {
+    pool.getConnection((err, conn) => { 
+        if(err) {
+            return fail(err);
+        }
+        let sql = "select * from hospital_01000000";
+        conn.query(sql, (err, rows) => {
+        if(err) {
+            return fail(err);
+        }
+        conn.release();
+        done(rows);
+        });
+    });
+};
+
+exports.getAllhospital00100000 = (fail, done) => {
+    pool.getConnection((err, conn) => { 
+        if(err) {
+            return fail(err);
+        }
+        let sql = "select * from hospital_00100000";
+        conn.query(sql, (err, rows) => {
+        if(err) {
+            return fail(err);
+        }
+        conn.release();
+        done(rows);
+        });
+    });
+};
+
+exports.getAllhospital00010000 = (fail, done) => {
+    pool.getConnection((err, conn) => { 
+        if(err) {
+            return fail(err);
+        }
+        let sql = "select * from hospital_00010000";
+        conn.query(sql, (err, rows) => {
+        if(err) {
+            return fail(err);
+        }
+        conn.release();
+        done(rows);
+        });
+    });
+};
+
+exports.getAllhospital00001000 = (fail, done) => {
+    pool.getConnection((err, conn) => { 
+        if(err) {
+            return fail(err);
+        }
+        let sql = "select * from hospital_00001000";
+        conn.query(sql, (err, rows) => {
+        if(err) {
+            return fail(err);
+        }
+        conn.release();
+        done(rows);
+        });
+    });
+};
+
+exports.getAllhospital00000100 = (fail, done) => {
+    pool.getConnection((err, conn) => { 
+        if(err) {
+            return fail(err);
+        }
+        let sql = "select * from hospital_00000100";
+        conn.query(sql, (err, rows) => {
+        if(err) {
+            return fail(err);
+        }
+        conn.release();
+        done(rows);
+        });
+    });
+};
+
+exports.getAllhospital00000010 = (fail, done) => {
+    pool.getConnection((err, conn) => { 
+        if(err) {
+            return fail(err);
+        }
+        let sql = "select * from hospital_00000010";
+        conn.query(sql, (err, rows) => {
+        if(err) {
+            return fail(err);
+        }
+        conn.release();
+        done(rows);
+        });
+    });
+};
+
+exports.getAllhospital00000001 = (fail, done) => {
+    pool.getConnection((err, conn) => { 
+        if(err) {
+            return fail(err);
+        }
+        let sql = "select * from hospital_00000001";
+        conn.query(sql, (err, rows) => {
+        if(err) {
+            return fail(err);
+        }
+        conn.release();
+        done(rows);
+        });
+    });
+};
